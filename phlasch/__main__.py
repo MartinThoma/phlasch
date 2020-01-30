939cccc2669f1f864b1c09698f36963718f425b9
Behzad B. Mokhtari <35877268+bbmokhtari@users.noreply.github.com>
2020-01-30 21:36:58 +0330

    Created the ability to run the package directly

---
+++ phlasch/__main__.py
@@ -0,0 +1,3 @@
+from phlasch.main import main
+
+main()
--- phlasch/main.py
+++ phlasch/main.py
@@ -2,6 +2,11 @@ from aiohttp.web import Application, run_app
 from phlasch.core.configure import configure as configure_core
 
 
-app = Application()
-configure_core(app)
-run_app(app)
+def main():
+    app = Application()
+    configure_core(app)
+    run_app(app)
+
+
+if __name__ == '__main__':
+    main()
