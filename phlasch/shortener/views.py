54194659f591998b212c8d2c789ebb919d0be07d
Behzad B. Mokhtari <35877268+bbmokhtari@users.noreply.github.com>
2020-01-31 13:50:37 +0330

    shortener validation in place

--- phlasch/shortener/views.py
+++ phlasch/shortener/views.py
@@ -1,5 +1,17 @@
 from aiohttp import web
+from phlasch.shortener.validators import validate_url
 
 
 async def shorten(request, data):
+    # validate data
+    address = data.get('address')
+    if not address:
+        return web.json_response({
+            'address': 'this field can not be blank.',
+        }, status=400)
+    if not validate_url(address):
+        return web.json_response({
+            'address': 'this field must be a valid url.',
+        }, status=400)
+
     return web.json_response(data)
