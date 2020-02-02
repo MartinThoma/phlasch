2bd3e2f0f5d0c9f0aff6cee7c8d9ce5d34fa5032
Behzad B. Mokhtari <35877268+bbmokhtari@users.noreply.github.com>
2020-02-02 22:14:47 +0330

    stats list and stats retrieve tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -15,3 +15,23 @@ async def test_redirector_redirect(cli, tables):
     response = await cli.get('/1', allow_redirects=False)
     assert response.status == 302
     assert response.headers['Location'] == 'https://google.com'
+
+
+async def test_stats_list(cli, tables):
+    response = await cli.get('/stats/list')
+    assert response.status == 200
+    response_json = await response.json()
+    assert response_json[0]['id'] == 1
+    assert response_json[0]['address'] == 'https://google.com'
+    assert response_json[0]['shortcut'] == '1'
+    assert response_json[0]['visits'] == 1
+
+
+async def test_stats_retrieve(cli, tables):
+    response = await cli.get('/stats/retrieve/1')
+    assert response.status == 200
+    response_json = await response.json()
+    assert response_json['id'] == 1
+    assert response_json['address'] == 'https://google.com'
+    assert response_json['shortcut'] == '1'
+    assert response_json['visits'] == 1
