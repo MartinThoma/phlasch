10fcb7690883b6d4ef1e6474007e0a67df3b6a70
Behzad B. Mokhtari <35877268+bbmokhtari@users.noreply.github.com>
2020-02-02 22:44:42 +0330

    start-tests.sh command to run the tests

---
+++ start-tests.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -o errexit
+
+random=`uuidgen | sed 's/-//g'`
+
+if [ "$1" != "--no-postgres-docker" ]; then
+    docker run --rm --name $random -d -p 5433:5432 postgres
+    export PHLASCH_DB_PORT=5433
+fi
+
+pytest
+
+if [ "$1" != "--no-postgres-docker" ]; then
+    docker stop $random
+fi
