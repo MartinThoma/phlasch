f5199a53b80c6f94320c9fce6b66efb1dc645798
Behzad B. Mokhtari <35877268+bbmokhtari@users.noreply.github.com>
2020-02-02 06:08:58 +0330

    setup.py first draft

---
+++ setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+
+with open('./.github/README.md', 'r') as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='phlasch-server',
+    version='',
+    description='a url shortener app/lib.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/bbmokhtari/phlasch-server',
+    author='Behzad B. Mokhtari',
+    author_email='behzad.public@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Framework :: AsyncIO',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet',
+    ],
+    keywords=' '.join(['url-shortener', 'url-shortener-microservice']),
+    project_urls={
+        'Documentation': 'https://github.com/bbmokhtari/phlasch-server',
+        'Source': 'https://github.com/bbmokhtari/phlasch-server',
+        'Tracker': 'https://github.com/bbmokhtari/phlasch-server/issues',
+    },
+    packages=setuptools.find_packages(
+        exclude=[
+            'tests',
+        ],
+    ),
+    python_requires='>=3.5, <4',
+)
