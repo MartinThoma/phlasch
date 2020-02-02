3100d35cd29de383bf8c8428cfa6a94f71389e7e
Behzad B. Mokhtari <35877268+bbmokhtari@users.noreply.github.com>
2020-02-02 14:44:07 +0330

    cleaner wsgi runners

--- start-all.sh
+++ start-all.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-python -m phlasch migrate db && gunicorn phlasch.runners:get_all_runnable --bind 0.0.0.0:8080 --worker-class aiohttp.GunicornWebWorker
+set -o errexit
+
+# migrate database
+python -m phlasch migrate db
+
+# run
+gunicorn phlasch.runners:get_all_runnable \
+    --bind 0.0.0.0:8080 \
+    --worker-class aiohttp.GunicornWebWorker
