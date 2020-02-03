bd6c86c9639e184c1936e51704e6e35333d1e785
Behzad B. Mokhtari <35877268+bbmokhtari@users.noreply.github.com>
2020-02-03 04:40:07 +0330

    simplified dev swarm

--- samples/dev/start.sh
+++ samples/dev/start.sh
@@ -12,8 +12,5 @@ docker build -t phlasch:dev $root_dir
 # pull postgres
 docker pull postgres
 
-# create database volume
-mkdir -p $current_dir/volumes/database
-
 # deploy swarm
 docker stack deploy -c $current_dir/docker-compose.yml phlasch_dev
